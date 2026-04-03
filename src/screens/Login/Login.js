5c89a972ed65fda8ffd5bed0e9852749a0b85c05
icaronni.ux <icaronni@udesa.edu.ar>
2026-04-03 18:06:24 -0300

    termine login, faltan cookies

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -11,7 +11,7 @@ class Login extends Component{
     }
 
     controlarCambios(event){
-        let valor = e.target.value;
+        let valor = event.target.value;
 
         if(event.target.name === "email"){
             this.setState({ email: valor });
@@ -21,23 +21,37 @@ class Login extends Component{
     }
 
     evitarSubmit(e){
-        e.preventDefault();}
+        e.preventDefault();
+    
+        let usuarios = JSON.parse(localStorage.getItem("usuariosGuardados")) || [];
+        for(let i = 0; i<usuarios.length; i++){
+            if( usuarios[i].email === this.state.email && usuarios[i].password === this.state.password)
+            {this.props.history.push("/home");
+                return;
+            }
+        }
+        this.setState({ error: "Credenciales incorrectas" });
+    }
 
     render(){
+        let mensajeError = "";
+        if (this.state.error !== ""){
+            mensajeError = <p>{this.state.error}</p>;
+        }
+
         return(
             <div>
                 <h2>Login</h2>
                 <form onSubmit={(e) => this.evitarSubmit(e)}>
-                    <input type="email" value={this.state.value} onChange={(e) => this.controlarCambios(e)} placeholder="Email"/>
+                    <input type="email" name="email" value={this.state.email} onChange={(e) => this.controlarCambios(e)} placeholder="Email"/>
                     <input type="password" name="password" value={this.state.password} onChange={(e) => this.controlarCambios(e)} placeholder="Password"/>
                     <button type="submit">Ingresar</button>
                 </form>
+                {mensajeError}
 
             </div>
         )
     }
-
-
 }
 
 export default Login;
