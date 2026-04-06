5f4209fab39604a09fb728c2deef98daeb6614f5
icaronni.ux <icaronni@udesa.edu.ar>
2026-04-06 02:00:20 -0300

    .

--- src/components/CardPeli/CardPeli.js
+++ src/components/CardPeli/CardPeli.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { withRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class CardPeli extends Component {
     constructor(props){
