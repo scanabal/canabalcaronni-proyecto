e7caa2ec352786492818e057dffe3699601b7f52
scanabal <scanabal@udesa.edu.ar>
2026-04-13 17:42:07 -0300

    fetches

--- src/components/DetalleTv/DetalleTv.js
+++ src/components/DetalleTv/DetalleTv.js
@@ -13,7 +13,7 @@ class Detalle extends Component{
     }
     componentDidMount () {
 const id= props.match.params.id
-fetch ("https://api.themoviedb.org/3/movie/{movie_id}?api_key=57f7a2a82d57f08ae3dba76c4340b8c0")
+fetch ("https://api.themoviedb.org/3/tv/{series_id}?api_key=57f7a2a82d57f08ae3dba76c4340b8c0")
 .then (response => response.json())
 .then (data => this.setState({data: data.results}))
 .catch (error => console.log(error))
