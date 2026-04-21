1ead78c74a5ff6694f02acd8536cead4f76cd353
scanabal <scanabal@udesa.edu.ar>
2026-04-20 23:17:08 -0300

    favortitos

--- src/screens/Favoritos/Favoritos.js
+++ src/screens/Favoritos/Favoritos.js
@@ -52,6 +52,103 @@ class Favoritos extends Component{
       })
         
 }
+  
+ eliminarPelicula(id) {
+    let nuevasPeliculas = this.state.peliculasFavoritas.filter(pelicula=> pelicula.id !== id);
+
+    let storage = JSON.parse(localStorage.getItem("favoritosPeliculas"));
+    let nuevosIds = storage.filter(fav => fav !== id);
+
+    localStorage.setItem("favoritosPeliculas", JSON.stringify(nuevosIds));
+
+    this.setState({ 
+      peliculasFavoritas: nuevasPeliculas 
+    });
+  }
+
+  eliminarSerie(id) {
+    let nuevasSeries = this.state.seriesFavoritas.filter(s => s.id !== id);
+
+    let storage = JSON.parse(localStorage.getItem("favoritosSeries"));
+    let nuevosIds = storage.filter(fav => fav !== id);
+
+    localStorage.setItem("favoritosSeries", JSON.stringify(nuevosIds));
+
+    this.setState({
+       seriesFavoritas: nuevasSeries 
+      });
+  }
+
+  render() {
+
+  let contenidoPeliculas;
+  let contenidoSeries;
+
+  if (this.state.peliculasFavoritas.length === 0) {
+    contenidoPeliculas = <p>No hay películas favoritas</p>;
+  } else {
+    contenidoPeliculas = (
+      <section className="PeliculasFav">
+        {this.state.peliculasFavoritas.map((pelicula, idx) => (
+          <section className="cardFav">
+                                <CardPeli
+                                    key={idx}
+                                    id={pelicula.id}
+                                    img={pelicula.poster_path}
+                                    title={pelicula.original_title}
+                                    overview={pelicula.overview}
+                                />
+
+              <button onClick={() => this.eliminarPelicula(pelicula.id)} className="eliminar">
+              Eliminar
+            </button>
+            
+          </section>
+        ))}
+      </section>
+     );
+   }
+
+    if (this.state.seriesFavoritas.length === 0) {
+     contenidoSeries = <p>No hay series favoritas</p>;
+   } else {
+     contenidoSeries = (
+       <section className="SeriesFav">
+         {this.state.seriesFavoritas.map((serie, idx) => (
+           <section className="cardFav">
+                                 <CardSerie
+                                     key={idx}
+                                     id={serie.id}
+                                     img={serie.poster_path}
+                                     title={serie.original_name}
+                                     overview={serie.overview}
+                                 />
+
+             <button onClick={() => this.eliminarSerie(serie.id)} className="eliminar">
+               Eliminar
+            </button>
+           </section>
+         ))}
+       </section>
+     );
+   }
+
+   return (
+     <main>
+       <h1>Favoritos</h1>
+
+       <section>
+         <h2>Películas favoritas</h2>
+          {contenidoPeliculas}
+       </section>
+
+      <section>
+        <h2>Series favoritas</h2>
+        {contenidoSeries}
+      </section>
+    </main>
+    );
+  }
 }
     
     
